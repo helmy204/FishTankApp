12a329f7dacf34716d3f2f18356509a0c714f1ff
helmy204 <hamadaelking204@hotmail.com>
2016-01-21 14:59:56 +0200

    Finish

--- FishTankApp/src/FishTankApp/Options/ThresholdOptions.cs
+++ FishTankApp/src/FishTankApp/Options/ThresholdOptions.cs
@@ -10,7 +10,7 @@ namespace FishTankApp.Options
         public int WaterTemperatureMax { get; set; }
         public int WaterTemperatureMin { get; set; }
         public int FishMotionMax { get; set; }
-        public int FishMothionMin { get; set; }
+        public int FishMotionMin { get; set; }
         public int WaterOpacityMax { get; set; }
         public int WaterOpacityMin { get; set; }
         public int LightIntensityMax { get; set; }
